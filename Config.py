eb71401220bdca8043cb8b76a4d38c209a61f1b8
wildbat333 <wildbat333@gmail.com>
2022-03-11 19:01:28 +0300

    4

--- Config.py
+++ Config.py
@@ -1,19 +1,9 @@
 import pandas as pd
-from pandas import options
-#import xlrd
+from openpyxl import Workbook
 
-options.io.excel.xlsx.writer = "xlwt"
-file_name = "smart_ivanovo_t.xls"
+file_name_input = "smart_ivanovo.xlsx"
+file_name_result = "smart_ivanovo_result.xlsx"
 
-# table_columns = {'Dates':["01.01.22"], 'Sale_manager':["Долг"],
-#                  'Code_shop':["Ч4688Ч4688"], 'Name_shop':["Магазин (Пел"],
-#                  'Code_position':["0D1-0700D1-070"], 'Name_position':['Семечки "Д'],
-#                  'Count_positions':[9], 'Sum_positions':[111.11]}
-
-tp = ["Долгачева Ирина (ЭК СМАРТ",
-      "Дроздова Марина (ЭК СМАРТ",
-      "Корсакова Елена (ЭК СМАРТ",
-      "Федотова Анна (ЭК СМАРТ)"]
 d = ['0D1-035', 'Семечки "Джинн"35г.(100)', 100,
      '0D1-070', 'Семечки "Джинн"70г.(50)', 50,
      '0D1-140', 'Семечки "Джинн"(10%)140г.(25)', 25,
@@ -46,18 +36,16 @@ k_s = ['0S2-040', 'Арахис "Караван орехов"Стандарт"40
 t = ['0T2-050', 'Семечки тыквы "Джинн"(соленые)50г.(70)', 70,
      '0T2-100', 'Семечки тыквы "Джинн"(соленые)100г.(35)', 35]
 
-djin = dict(Name='Семечки "Джинн"70г.(50)', Key='0D1-070', Koef='50')
+wb = Workbook()
+ws = wb.active
+ws1 = wb.create_sheet('Сводка')
+ws2 = wb.create_sheet('Пачки')
+ws3 = wb.create_sheet('Полная')
+wb.remove(ws)
+wb.save(file_name_result)
 
-djin_sol = []
 
-#wb = xlrd.open_workbook(file_name, encoding_override='CORRECT_ENCODING')
-df = pd.read_excel(file_name, header=None)
-print(df.head(5))
-
-df.info()
-
-summm = df.loc[1, 6]
-print(summm)
+df = pd.read_excel(file_name_input, header=None)
 
 group_koef = []
 group_koef.extend(d)
@@ -69,13 +57,13 @@ group_koef.extend(m)
 group_koef.extend(k)
 group_koef.extend(k_s)
 group_koef.extend(t)
-#print(group_koef)
+
 
 for i, row in df.iterrows():
     for f in range(len(group_koef)):
         if df.loc[i, 4] == group_koef[f]:
             df.loc[i, 8] = group_koef[f+2]
-            df.loc[i, 9] = round(df.loc[i, 6] / df.loc[i, 8], 2)
+            df.loc[i, 9] = df.loc[i, 6] / df.loc[i, 8]
             if df.loc[i, 4] in d: df.loc[i, 10] = 'Джин'
             if df.loc[i, 4] in d_s: df.loc[i, 10] = 'Джин соль'
             if df.loc[i, 4] in v: df.loc[i, 10] = 'Великан'
@@ -87,48 +75,23 @@ for i, row in df.iterrows():
             if df.loc[i, 4] in t: df.loc[i, 10] = 'Тыква'
         else:
             f = f+3
-print(df.head(15))
 
-agg_func_sum = {9:['sum']}
-svod_df = df.groupby([1, 10]).agg(agg_func_sum)
 
 agg_func_sum = {6:['sum']}
 prom_df = df.groupby([1, 4, 5]).agg(agg_func_sum)
-prom_df.info()
-#for i in range(d):
 
-#df = df.loc[~df['STP'].isin([1005092])]
-#prom_df1 = prom_df.iloc[:, 1].isin(["Долгачева Ирина (ЭК СМАРТ"])
-#print(prom_df1)
-d_result = [100, 200, 500, 750]
-slov = {'d':d_result}
-df_result = pd.DataFrame(slov, index=tp)
-print(df_result)
-svod_df.to_excel("smart_ivanovo_svod.xls", sheet_name='Сводка', startrow=0, merge_cells=False)
-prom_df.to_excel("smart_ivanovo_packs.xls", sheet_name='Пачки', startrow=0, merge_cells=False)
-df.to_excel("smart_ivanovo_all.xls", sheet_name='Полная', startrow=0, merge_cells=False)
+svod_df = pd.crosstab(df[1],
+                      df[10],
+                      values=df[9],
+                      aggfunc='sum',
+                      normalize=False)
+svod_df = svod_df.round(2)
 
-#df[df[1] == tp[0]]
 
-#print(df.last_valid_index)
-#keys = []
-#for index in df.itertuples():
-#keys[df.columns[4]] = df.columns[5]
-    #df.columns[5]
-#print(keys)
-#df.iterrows()
-#n = 0
-# for row in df.itertuples(index=False):
-#     keys.append(df.columns[4])
-#     n = n +1
-#     #print(row)
-# print(n)
-# print(keys)
-#for row in df.iterrows():
-	#print(f"{row}")
-# tps = []
-# for row in df.itertuples(index=False):
-#     tps.append(df.columns[1])
-# print(tps)
-# tp_b = list(set(tps))
-# print(tp_b)
+with pd.ExcelWriter(file_name_result, mode="a", engine="openpyxl", if_sheet_exists="replace") as writer:
+    svod_df.to_excel(writer, sheet_name='Сводка', startrow=0)
+    prom_df.to_excel(writer, sheet_name='Пачки')
+    df.to_excel(writer, sheet_name='Полная')
+
+
+
